b11265285e5863ec5cf7d747544c6e5fe8e12f70
Nathan Pillai <natepill@gmail.com>
2019-10-28 10:50:47 -0700

    [added] insertion sort

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -42,12 +42,14 @@ def is_sorted(items):
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
-    TODO: Running time: O(n^2) If the entire list is out of order, then we would have to do bubble sort (n)
-    times for (n) values in the list
+    TODO: Running time: O(n*m), Worst cast is O(n^2) If the entire list is out of order, then we would have to do bubble sort (n)
+    times for (n) values in the list. Additional O(n) to check if sorted
     TODO: Memory usage: O(1) because we just allocated memory for a couple variables"""
 
     index_1 = 0
     index_2 = 1
+
+    # TODO: keep counter for swaps for early exit
 
     # Bubble sort until the list is sorted
     while is_sorted(items) == False:
@@ -57,7 +59,7 @@ def bubble_sort(items):
 
             # if first val is greater than second val:
                 #swap the values
-            if items[index_1] > items[index_2]:
+            if items[index_1] > items[index_2]: # O(1)
                 temp  = items[index_1]
                 items[index_1] = items[index_2]
                 items[index_2] = temp
@@ -77,8 +79,9 @@ def bubble_sort(items):
 def selection_sort(items):
     """Sort given items by finding minimum item, swapping it with first
     unsorted item, and repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    Running time: O(n*m) Best case O(n) with only a few swaps, but could be O(n^2) if values
+    at every position need to be swapped
+    Memory usage: O(1) Not really allocating much memory besides for variables """
 
     # init curr_index
     # init swap_index, value
@@ -91,36 +94,19 @@ def selection_sort(items):
         # increment curr_index
         # reset swap_index to curr_index + 1 and swap value to list value at curr_index+1
 
-    curr_index = 0
-    swap_index = 1
+    # iterate over range of indicies
+    for index in range(len(items)):
 
-    # index, value of smallest value seen so far
-    swap_info = [1, items[1]]
+        # find the smallest value in the list from the current index
+        smallest_index = index
+        # iterate over range of indicies from current index to end of list
+        for small_index in range(index+1, len(items)):
+            if items[smallest_index] > items[small_index]:
+                smallest_index = small_index
 
-    while is_sorted(items) == False or curr_index < len(items)-1:
+        # Swap current index with smallest element
+        items[index], items[smallest_index] = items[smallest_index], items[index]
 
-        # Find smallest value from the current index onwards
-        while swap_index < len(items)-1:
-
-            # found smaller value than current value (potential swap)
-            if items[curr_index] > items[swap_index]:
-                # Smaller value is the smallest value seen so far
-                if items[swap_index] < swap_info[1]:
-                    # Assigned smallest value so far to current value at swap_index
-                    swap_info[0] = swap_index
-                    swap_info[1] = items[swap_index]
-
-            swap_index += 1
-
-        # NOTE: Should not swap if no smaller value found
-        # swap value at curr_index with value at swap_index
-        temp = items[swap_info[0]]
-        items[swap_info[0]] = items[curr_index]
-        items[curr_index] = temp
-        curr_index += 1
-        # reset swap_index to curr_index + 1 and swap value to list value at curr_index+1
-        swap_info[0] = curr_index+1
-        swap_info[1] = items[curr_index+1]
 
 
     # TODO: Repeat until all items are in sorted order
@@ -133,6 +119,37 @@ def insertion_sort(items):
     order in front of items, and repeating until all items are in order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
+
+    # iterate over range of indicies
+
+    # compare the value of current index with preceding values at prior indicies
+    # insert current value at index whose value is smaller than current value
+
+    # Skip first index in index iteration range because first value is "sorted"
+    for index in range(1, len(items)):
+        # Iterate over preceding indicies
+        for prev_index in range(index, -1, -1):
+            # Point of insertion
+            if items[index] > items[prev_index]:
+                items.insert(index, items[index])
+
+        # No more values to compare to, so current value is the smallest value seen so far
+        if items[index] < items[0]:
+            items.insert(0, items[index])
+
+
+        # while index > 0 or items[index-1] > items[index]:
+        #     if items[index] < items[insert_index]:
+        #         index -= 1
+
+        #
+        # insert_index = index-1
+        # # Keep comparing until reached begining of list
+        # while index > 0:
+        #
+        #     if items[index] < items[insert_index]:
+
+
     # TODO: Repeat until all items are in sorted order
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
