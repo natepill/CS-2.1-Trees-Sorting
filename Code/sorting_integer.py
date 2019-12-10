24bc356e6465566ac2940398e8f3711fc1ea43cd
Nathan Pillai <natepill@gmail.com>
2019-12-10 10:23:49 -0800

    [added] count sort

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -6,10 +6,25 @@ def counting_sort(numbers):
     then looping over counts and copying that many numbers into output list.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum integer values)
-    # TODO: Create list of counts with a slot for each number in input range
-    # TODO: Loop over given numbers and increment each number's count
-    # TODO: Loop over counts and append that many numbers into output list
+    # Find range of given numbers (minimum and maximum integer values)
+    min, max = min(numbers), max(numbers)
+
+    # Create list of counts with a slot for each number in input range
+    count_list = {}
+    # Loop over given numbers and increment each number's count
+    for num in range(min, max+1):
+        if num in count_list:
+            count_list[num] += 1
+        else:
+            count_list[num] = 1
+
+    output_list = []
+
+    # Loop over counts and append that many numbers into output list
+    for key, value in count_list:
+        while value > 0:
+            output_list.append(key)
+
     # FIXME: Improve this to mutate input instead of creating new output list
 
 
