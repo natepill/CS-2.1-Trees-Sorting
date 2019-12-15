55d3d9199ad89890b41a7145029677ea5eeeecdb
Nathan Pillai <natepill@gmail.com>
2019-12-14 17:59:48 -0800

    [debugged inesert avl]

---
+++ Code/avl_tree.py
@@ -0,0 +1,84 @@
+class node:
+	def __init__(self,value=None):
+		self.value=value
+		self.left_child=None
+		self.right_child=None
+		self.parent=None # pointer to parent node in tree
+		self.height=1 # height of node in tree (max dist. to leaf) NEW FOR AVL
+
+class AVLTree:
+	def __init__(self):
+		self.root=None
+
+	def __repr__(self):
+		if self.root==None: return ''
+		content='\n' # to hold final string
+		cur_nodes=[self.root] # all nodes at current level
+		cur_height=self.root.height # height of nodes at current level
+		sep=' '*(2**(cur_height-1)) # variable sized separator between elements
+		while True:
+			cur_height+=-1 # decrement current height
+			if len(cur_nodes)==0: break
+			cur_row=' '
+			next_row=''
+			next_nodes=[]
+
+			if all(n is None for n in cur_nodes):
+				break
+
+			for n in cur_nodes:
+
+				if n==None:
+					cur_row+='   '+sep
+					next_row+='   '+sep
+					next_nodes.extend([None,None])
+					continue
+
+				if n.value!=None:
+					buf=' '*int((5-len(str(n.value)))/2)
+					cur_row+='%s%s%s'%(buf,str(n.value),buf)+sep
+				else:
+					cur_row+=' '*5+sep
+
+				if n.left_child!=None:
+					next_nodes.append(n.left_child)
+					next_row+=' /'+sep
+				else:
+					next_row+='  '+sep
+					next_nodes.append(None)
+
+				if n.right_child!=None:
+					next_nodes.append(n.right_child)
+					next_row+='\ '+sep
+				else:
+					next_row+='  '+sep
+					next_nodes.append(None)
+
+			content+=(cur_height*'   '+cur_row+'\n'+cur_height*'   '+next_row+'\n')
+			cur_nodes=next_nodes
+			sep=' '*int(len(sep)/2) # cut separator size in half
+		return content
+
+	def insert(self,value):
+		if self.root==None:
+			self.root=node(value)
+		else:
+			self._insert(value,self.root)
+
+	def _insert(self,value,cur_node):
+		if value<cur_node.value:
+			if cur_node.left_child==None:
+				cur_node.left_child=node(value)
+				cur_node.left_child.parent=cur_node # set parent
+				self._inspect_insertion(cur_node.left_child)
+			else:
+				self._insert(value,cur_node.left_child)
+		elif value>cur_node.value:
+			if cur_node.right_child==None:
+				cur_node.right_child=node(value)
+				cur_node.right_child.parent=cur_node # set parent
+				self._inspect_insertion(cur_node.right_child)
+			else:
+				self._insert(value,cur_node.right_child)
+		else:
+			print("Value already in tree!")
